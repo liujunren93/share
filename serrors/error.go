6555adbd5d5625a145d62663fa99726a4c859602
liujunren93 <liujunrenemail@qq.com>
2020-09-16 17:46:52 +0800

    deg

--- serrors/error.go
+++ serrors/error.go
@@ -12,7 +12,7 @@ func New(code int32, msg string) error {
 
 func BadRequest(err error) error {
 	msg := http.StatusText(400)
-	if err == nil {
+	if err != nil {
 		msg = err.Error()
 	}
 	return status.New(codes.Code(400), msg).Err()
@@ -22,7 +22,7 @@ func BadRequest(err error) error {
 // Unauthorized generates a 401 Error.
 func Unauthorized(err error) error {
 	msg := http.StatusText(401)
-	if err == nil {
+	if err != nil {
 		msg = err.Error()
 	}
 	return status.New(codes.Code(401), msg).Err()
@@ -31,25 +31,25 @@ func Unauthorized(err error) error {
 // Forbidden generates a 403 Error.
 func Forbidden(err error) error {
 	msg := http.StatusText(403)
-	if err == nil {
+	if err != nil {
 		msg = err.Error()
 	}
-	return status.New(codes.Code(403),msg).Err()
+	return status.New(codes.Code(403), msg).Err()
 }
 
 // NotFound generates a 404 Error.
 func NotFound(err error) error {
 	msg := http.StatusText(404)
-	if err == nil {
+	if err != nil {
 		msg = err.Error()
 	}
-	return status.New(codes.Code(404),msg).Err()
+	return status.New(codes.Code(404), msg).Err()
 }
 
 // MethodNotAllowed generates a 405 Error.
 func MethodNotAllowed(err error) error {
 	msg := http.StatusText(405)
-	if err == nil {
+	if err != nil {
 		msg = err.Error()
 	}
 	return status.New(codes.Code(405), msg).Err()
@@ -58,7 +58,7 @@ func MethodNotAllowed(err error) error {
 // Timeout generates a 408 Error.
 func Timeout(err error) error {
 	msg := http.StatusText(408)
-	if err == nil {
+	if err != nil {
 		msg = err.Error()
 	}
 	return status.New(codes.Code(408), msg).Err()
@@ -67,7 +67,7 @@ func Timeout(err error) error {
 // Conflict generates a 409 Error.
 func Conflict(err error) error {
 	msg := http.StatusText(405)
-	if err == nil {
+	if err != nil {
 		msg = err.Error()
 	}
 	return status.New(codes.Code(409), msg).Err()
@@ -76,7 +76,8 @@ func Conflict(err error) error {
 // InternalServerError generates a 500 Error.
 func InternalServerError(err error) error {
 	msg := http.StatusText(500)
-	if err == nil {
+	if err != nil {
+
 		msg = err.Error()
 	}
 	return status.New(codes.Code(500), msg).Err()
